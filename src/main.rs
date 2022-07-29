b00c0b8fc3317b76d512b6a3a1c1e8e71759509b
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-07-29 00:48:01 -0300

    looping para mais de um comando

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use std::io::stdin;
+use std::io::{stdin, stdout, Write};
+
 use std::process::Command;
 
 fn main() {
-    let mut input:String = String::new();
-    
-    stdin().read_line(&mut input).unwrap();
-  
-    // reading new line leaves a trailing new line.
-  
-    let command = input.trim();
-  
-    Command::new(command)
-      .spawn()
-      .unwrap();
+    loop {
+        print!("> ");
+        stdout().flush().ok();
+
+        let mut input:String = String::new();
+
+        stdin().read_line(&mut input).unwrap();
+
+        // reading new line leaves a trailing new line.
+        let command = input.trim();
+        let mut child = Command::new(command)
+          .spawn()
+          .unwrap();
+
+        child.wait().ok();
+    }
   }
