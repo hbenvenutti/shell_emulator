399f071c0a2d0157e70dfcec913bff61c5c495e2
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-07-29 00:19:16 -0300

    o terminal aceita um comando

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::stdin;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
-}
+    let mut input:String = String::new();
+    
+    stdin().read_line(&mut input).unwrap();
+  
+    // reading new line leaves a trailing new line.
+  
+    let command = input.trim();
+  
+    Command::new(command)
+      .spawn()
+      .unwrap();
+  }
