660fd8477118007a625524b576e5251f3285eb69
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-07-29 01:35:06 -0300

    implementado o comando cd

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,42 @@ use std::io::{stdin, stdout, Write};
 
 use std::process::Command;
 
+use std::env;
+use std::path::Path;
+
 fn main() {
-    loop {
-        print!("> ");
-        stdout().flush().ok();
+  loop {
+    print!("> ");
+    stdout().flush().ok();
 
-        let mut input:String = String::new();
+    let mut input:String = String::new();
 
-        stdin().read_line(&mut input).unwrap();
-        // reading new line leaves a trailing new line.
+    stdin().read_line(&mut input).unwrap();
+    // reading new line leaves a trailing new line.
 
-        let mut parts = input.trim().split_whitespace();
+    let mut parts = input.trim().split_whitespace();
 
-        let command = parts.next().unwrap();
-        let args = parts;
+    let command = parts.next().unwrap();
+    let args = parts;
 
+    match command {
+      "cd" => {
+        let new_dir = args.peekable().peek().map_or("/", |x| *x);
+        let root = Path::new(new_dir);
         
+        if let Err(e) = env::set_current_dir(&root){
+          eprintln!("{}", e);
+        }
+      },
+
+      command => {
         let mut child = Command::new(command)
           .args(args)
           .spawn()
           .unwrap();
 
         child.wait().ok();
+      }
     }
   }
+}
