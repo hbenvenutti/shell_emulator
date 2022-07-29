b6ccfd8173c5bb00036687dc445ab79aff20b1ee
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-07-29 01:57:21 -0300

    builtin exit

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,20 @@ fn main() {
     stdin().read_line(&mut input).unwrap();
     // reading new line leaves a trailing new line.
 
+    // -- Argumentos ---------------------------------------------------------------------------- //
+    // Divide o input em comando e argumentos
     let mut parts = input.trim().split_whitespace();
 
     let command = parts.next().unwrap();
     let args = parts;
 
-    match command {
+
+    match command {      
+      // -- Builtin cd -------------------------------------------------------------------------- //
       "cd" => {
         let new_dir = args.peekable().peek().map_or("/", |x| *x);
+        // executar cd sem especificar um diretório muda para o '/'.
+
         let root = Path::new(new_dir);
         
         if let Err(e) = env::set_current_dir(&root){
@@ -30,13 +36,17 @@ fn main() {
         }
       },
 
+      // -- Fechar o terminal ------------------------------------------------------------------- //
+      "exit" => return,
+
+      // -- Execução dos comandos --------------------------------------------------------------- //
       command => {
         let mut child = Command::new(command)
           .args(args)
           .spawn()
           .unwrap();
 
-        child.wait().ok();
+        child.wait().ok(); // Não deixa dois comandos executarem ao mesmo tempo.
       }
     }
   }
