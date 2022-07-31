97e21979566617fc8e5d5de0bb65bf6792ba547e
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-07-30 23:10:53 -0300

    comandos com pipe

--- src/main.rs
+++ src/main.rs
@@ -9,52 +9,73 @@ fn main() {
   loop {
     print!("> ");
     stdout().flush().ok();
-
+  
     let mut input:String = String::new();
-
+  
     stdin().read_line(&mut input).unwrap();
     // reading new line leaves a trailing new line.
-
-    // -- Argumentos ---------------------------------------------------------------------------- //
-    // Divide o input em comando e argumentos
-    let mut parts = input.trim().split_whitespace();
-
-    let command = parts.next().unwrap();
-    let args = parts;
-
-
-    match command {      
-      // -- Builtin cd -------------------------------------------------------------------------- //
-      "cd" => {
-        let new_dir = args.peekable().peek().map_or("/", |x| *x);
-        // executar cd sem especificar um diretório muda para o '/'.
-
-        let root = Path::new(new_dir);
-        
-        if let Err(e) = env::set_current_dir(&root){
-          eprintln!("{}", e);
-        }
-      },
-
-      // -- Fechar o terminal ------------------------------------------------------------------- //
-      "exit" => return,
-
-      // -- Execução dos comandos --------------------------------------------------------------- //
-      command => {
-        let mut child = Command::new(command)
-          .args(args)
-          .spawn();
-          // .unwrap();
-
-        match child {
+  
+    
+    let mut commands = input.trim().split("|").peekable();
+    let mut previous_command = None;
+  
+    while let Some(command) = commands.next() {
+  
+      // -- Argumentos ---------------------------------------------------------------------------- //
+      // Divide o input em comando e argumentos
+      let mut parts = input.trim().split_whitespace();
+  
+      let command = parts.next().unwrap();
+      let args = parts;
+      
+      match command {      
+        // -- Builtin cd -------------------------------------------------------------------------- //
+        "cd" => {
+          let new_dir = args.peekable().peek().map_or("/", |x| *x);
+          // executar cd sem especificar um diretório muda para o '/'.
           
-          Ok(mut child) => { child.await() },
-          // igual ao código original, mas dá erro.
-
-          Err(e) => eprintln!("{}", e),
+          let root = Path::new(new_dir);
+          
+          if let Err(e) = env::set_current_dir(&root){
+            eprintln!("{}", e);
+          }
+        },
+        
+        // -- Fechar o terminal ------------------------------------------------------------------- //
+        "exit" => return,
+  
+        // -- Execução dos comandos --------------------------------------------------------------- //
+        command => {
+          let stdin = previous_command
+            .map_or(
+              Stdio::inherit(), 
+              |output: Child| Stdio::from(output.stdout.unwrap())
+            );
+  
+          let stdout = if commands.peek().is_some() {
+            Stdio::piped();
+          } else {
+              Stdio::inherit();
+          };
+  
+          let output = Command::new(command)
+          .args(args)
+          .stdin(stdin)
+          .stdout(stdout)
+          .spawn();
+  
+          match output {
+            Ok(output) => {previous_command = Some(output);},
+            Err(e) => {
+              previous_command = None;
+              eprintln!("{}", e);
+            }
+          }
         }
-        // child.wait().ok(); // Não deixa dois comandos executarem ao mesmo tempo.
-      }
+      }    
+    }
+    if let Some(mut final_command) = previous_command {
+      final_command.wait().unwrap();
     }
   }
 }
